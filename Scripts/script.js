6be547f73956a2180a1186104a28e881ad1f2e04
AlexandrGusIS221 <Sashag3174@gmail.com>
2024-11-06 02:47:32 +0500

    Добавил бургер

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,3 +7,28 @@ function toggleActive(selectedItem) {
 
     selectedItem.classList.add('catalog-filtr__item--active');
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Отменяем стандартное поведение ссылки
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = 124; // Смещение на 124 пикселя
+        const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth' // Плавная прокрутка
+        });
+    });
+});
+const button = document.getElementById('burger');
+const burgerlist = document.getElementById('burgerList');
+
+button.addEventListener('click', function() {
+    if(burgerlist.style.display == 'flex'){
+        burgerlist.style.display = 'none';
+    }
+    else{
+        burgerlist.style.display = 'flex';
+    }
+});
